fc65685a6b1f1115c7a3c4babe9cb92ef63b50c2
sanjay321654 <sanjayilaya2002@gmail.com>
2023-09-15 18:36:07 +0530

    updated the main.py with list comprehension

--- day-25-us-states-game-start/main.py
+++ day-25-us-states-game-start/main.py
@@ -7,26 +7,27 @@ screen.bgpic("blank_states_img.gif")
 
 data = pandas.read_csv("50_states.csv")
 all_states_data = data.state.to_list()
-list_states = []
-while len(list_states) < 50:
-    guessed_state = screen.textinput(title=f"{len(list_states)}/50 States Correct",
-                                     prompt="What's the another state").title()
+guessed_states = []
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's the another state").title()
 
-    if guessed_state == "Exit":
-        missing_states = []
-        for state in all_states_data:
-            if state not in list_states:
-                missing_states.append(state)
+    if answer_state == "Exit":
+        missing_states = [state for state in all_states_data if state not in guessed_states]
+        # for state in all_states_data:
+        #     if state not in list_states:
+        #         missing_states.append(state)
+
         missing_data = pandas.DataFrame(missing_states)
         missing_data.to_csv("states_to_learn.csv")
         break
 
-    if guessed_state in all_states_data:
-        list_states.append(guessed_state)
+    if answer_state in all_states_data:
+        guessed_states.append(answer_state)
         tim = Turtle()
         tim.hideturtle()
         tim.penup()
-        state_data = data[data.state == guessed_state]
+        state_data = data[data.state == answer_state]
         tim.goto(x=int(state_data.x), y=int(state_data.y))
-        tim.write(guessed_state)
+        tim.write(answer_state)
 
